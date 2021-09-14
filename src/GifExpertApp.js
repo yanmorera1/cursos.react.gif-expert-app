46a44cb199dab5ea6806b38aa713dc322e05e90b
Yan Carlos Morera <yan.morera@finanzauto.com.co>
2021-09-14 10:20:02 -0500

    Cambios en GifExpertAPP

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import AddCategory from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories(['Yansito', ...categories]);
+    //     setCategories(categs => [...categs, 'Yansito']);
+    // }
+
+    return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+
+        <hr/>
+
+        <ol>
+            {
+                categories.map(category => 
+                    <GifGrid 
+                        category={category}
+                        key={category}
+                    />
+                )
+            }
+        </ol>
+    </>
+    );
+}
+
+export default GifExpertApp;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GifExpertApp from './GifExpertApp';
+import "animate.css";
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
